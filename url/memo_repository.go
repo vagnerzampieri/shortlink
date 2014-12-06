ce4c2656b69d168863299bc9a0bdb2df4c219a1e
Vagner Zampieri <vgzampieri@gmail.com>
2014-12-06 18:42:39 +0000

    Create NewMemoRepository

--- url/memo_repository.go
+++ url/memo_repository.go
@@ -4,3 +4,10 @@ type memoRepository struct {
 	urls   map[string]*Url
 	clicks map[string]int
 }
+
+func NewMemoRepository() *memoRepository {
+	return &memoRepository{
+		make(map[string]*Url),
+		make(map[string]int),
+	}
+}
