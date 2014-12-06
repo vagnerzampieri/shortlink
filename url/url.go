2297ac010913ff037d2d95e6f90bfdb3717a78c1
Vagner Zampieri <vgzampieri@gmail.com>
2014-12-06 18:05:36 +0000

    Adding struct URL

--- url/url.go
+++ url/url.go
@@ -10,3 +10,13 @@ const (
 	length  = 5
 	symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_-+"
 )
+
+//type Repository interface {
+
+//}
+
+type Url struct {
+	Id          string    `json:"id"`
+	Criation    time.Time `json:"criation"`
+	Destination string    `json:"destination"`
+}
